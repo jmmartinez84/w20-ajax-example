e0b4aad9031bb213d76ca86b7ef4988ed27695b4
JOSE MANUEL MARTINEZ LAINEZ - E437892 <E437892@inetpsa.com>
2018-07-05 17:04:52 +0200

    First commit

---
+++ ajax/modules/main.js
@@ -0,0 +1,37 @@
+define([
+    '{angular}/angular',
+], function(angular) {
+    'use strict';
+
+    var module = angular.module('ajax', ['ngResource']);
+    module.service('DataService', ['$http', '$q', '$timeout', function($http, $q, $timeout){
+        var getData = function(){
+            var deferred = $q.defer();
+            $http.get('http://jsonplaceholder.typicode.com/users').then(function(response){
+                $timeout(function(){
+                    deferred.promise.resolve(response.data);
+                },2000);
+               
+            });
+            return deferred.promise;
+        }
+        return {
+            getData: getData
+        }
+    }]);
+    module.controller('ContentController',['$scope', 'DataService', function($scope, dataService){
+        $scope.getData = function(){
+            $scope.btnDisabled = true;
+            console.debug('get data');
+            $scope.data = [];
+            dataService.getData().then(function(result){
+                console.debug('got data!');
+                $scope.data = result;
+                $scope.btnDisabled = false;
+            })
+        }
+    }]);
+	return {
+		angularModules : [ 'ajax' ]
+	};
+});
